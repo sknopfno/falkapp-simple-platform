b88ded2d948e154b3ffbeaf4f2f99bec09216ca3
sknopfno <ryounassu@gmail.com>
2024-09-11 17:44:30 +0300

    Text deleted title

--- pages/index.js
+++ pages/index.js
@@ -11,9 +11,9 @@ export default function Home() {
       </Head>
 
       <main>
-        <Header title="Welcome to my app!" />
+        <Header title="Epmty header" />
         <p className="description">
-          Get started by editing <code>pages/index.js</code>
+          Fill me up bitch
         </p>
       </main>
 
