5096ff32a271e5a722bbf97bb6e0a2bf23105d03
sknopfno <ryounassu@gmail.com>
2024-10-04 12:19:22 +0300

    Test local on caht labda renormalize 2

--- netlify/functions/upload-to-drive/main.go
+++ netlify/functions/upload-to-drive/main.go
@@ -2,23 +2,40 @@ package main
 
 import (
 	"context"
-	"fmt"
+	"encoding/json"
 
 	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
 )
 
-func handler(ctx context.Context, request events.APIGatewayProxyRequest) (*events.APIGatewayProxyResponse, error) {
-	fmt.Println("This message will show up in the CLI console.")
+// Response represents the response format for the lambda function
+type Response struct {
+	Message string `json:"message"`
+}
 
-	return &events.APIGatewayProxyResponse{
-		StatusCode:      200,
-		Headers:         map[string]string{"Content-Type": "text/plain"},
-		Body:            "Upload to Google Drive serverless function connected API Gateaway (;0)",
-		IsBase64Encoded: false,
+// Handler is the lambda entry point
+func Handler(ctx context.Context, req events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
+	response := Response{
+		Message: "API Gateaway ready labda function ready ;)",
+	}
+
+	body, err := json.Marshal(response)
+	if err != nil {
+		return events.APIGatewayProxyResponse{
+			StatusCode: 500,
+			Body:       "Internal Server Error",
+		}, err
+	}
+
+	return events.APIGatewayProxyResponse{
+		StatusCode: 200,
+		Body:       string(body),
+		Headers: map[string]string{
+			"Content-Type": "application/json",
+		},
 	}, nil
 }
 
 func main() {
-	lambda.Start(handler)
+	lambda.Start(Handler)
 }
